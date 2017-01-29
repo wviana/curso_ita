f13cfdaa76520640981203e0db347d68f5d085e0
vinicius rodrigues silva <viniciusrodrigues93@yahoo.com.br>
2017-01-29 12:08:16 -0200

    imc criando construtor

---
+++ imc/src/imc/Paciente.java
@@ -0,0 +1,14 @@
+package imc;
+
+public class Paciente {
+	double peso;                 // atributos da classe paciente
+	double altura;
+	double metros;
+	
+	public Paciente(double peso, double altura , double metros){// construtor da classe paciente
+	 this.peso=peso;
+	 this.altura=altura;
+	 this.metros=metros;
+	}
+}	
+	
---
+++ imc/src/imc/Principal.java
@@ -0,0 +1,12 @@
+package imc;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Paciente vinicius = new Paciente(0.1, 0.2, 0.3);
+
+
+	}
+
+}
